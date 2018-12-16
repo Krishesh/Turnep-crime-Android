bcc3984000135ab86df2fa36d9d2c704e81877ec
Krishesh shrestha <shrestha.krishesh@gmail.com>
2018-12-16 09:58:03 +0545

    update

--- app/src/main/java/com/example/shres/nbdemo2/Fragements/BottomNavBar/Profile.java
+++ app/src/main/java/com/example/shres/nbdemo2/Fragements/BottomNavBar/Profile.java
@@ -1,18 +1,13 @@
 package com.example.shres.nbdemo2.Fragements.BottomNavBar;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import com.example.shres.nbdemo2.LoginAndRegistration.LoginActivity;
-import com.example.shres.nbdemo2.LoginAndRegistration.SignUpActivity;
 import com.example.shres.nbdemo2.R;
-import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
--- app/src/main/java/com/example/shres/nbdemo2/LoginAndRegistration/LoginActivity.java
+++ app/src/main/java/com/example/shres/nbdemo2/LoginAndRegistration/LoginActivity.java
@@ -1,10 +1,9 @@
 package com.example.shres.nbdemo2.LoginAndRegistration;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
